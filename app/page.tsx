c41c36c9f963bbc40e3767143cf4c365a6fdec65
Rahil Khan <hi.rahilkhan@gmail.com>
2023-12-07 01:05:45 +0530

    Added Skills section to my portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/main/Hero';
+import Skills from '@/components/main/Skills'
 import Image from 'next/image';
 
 export default function Home() {
   return(
     <main className='h-full w-full'>
-      <div className='flex flex-col h-[821px] gap-20'> 
+      <div className='flex flex-col  gap-20'> 
       <Hero />
+      <Skills />
       </div>
     </main>
   ); 
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -26,7 +26,7 @@ const HeroContent = () => {
         className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w[600px] w-auto h-auto'>
           <span>Providing
           <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 '> the best </span>
-          project experience</span>
+          project exprience</span>
         </motion.div>
 
         <motion.p
