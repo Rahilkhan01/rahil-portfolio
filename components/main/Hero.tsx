d27d6fd0f8f7532f46c750ae29cae46e7602532c
Rahil Khan <hi.rahilkhan@gmail.com>
2023-12-06 22:30:56 +0530

    Added Navbar to the portfolio

---
+++ components/main/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeroContent from '../sub/HeroContent';
+
+const Hero = () => {
+  return (
+    <div className='relative flex flex-col h-full w-full'>
+        <video 
+        autoPlay
+        muted
+        loop
+        className='rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover'>
+            <source src='/blackhole.webm' type='video/webm' />
+        </video>
+        <HeroContent/>
+    </div>
+  )
+};
+export default Hero;
