1f73bf6f3c8961dafee4d737168916011726b1a8
Rahil Khan <hi.rahilkhan@gmail.com>
2023-12-07 18:43:01 +0530

    Added Projects and cards components

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,12 +3,12 @@ import HeroContent from '../sub/HeroContent';
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
+    <div className='relative flex flex-col h-full w-full' id="about me">
         <video 
         autoPlay
         muted
         loop
-        className='rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover'>
+        className='rotate-180 absolute top-[-290px] h-full w-full left-0 z-[1] object-cover'>
             <source src='/blackhole.webm' type='video/webm' />
         </video>
         <HeroContent/>
